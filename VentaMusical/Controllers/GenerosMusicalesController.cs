4b0af444aca79aceecc1d26a12e5668b8a2532b2
Daniel Poveda <danielpovedaromero@gmail.com>
2024-06-26 17:46:08 -0600

    Se agregan models css , html y consulta de generos

---
+++ VentaMusical/Controllers/GenerosMusicalesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using VentaMusical.Models;
+using VentaMusical.Models.ViewModels;
+
+namespace VentaMusical.Controllers
+{
+    public class GenerosMusicalesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            var listaGenerosMusicales = new List<GenerosMusicalesViewModel>();
+
+            try
+            {
+                using (VentaMusicalDBEntities db = new VentaMusicalDBEntities())
+                {
+                    var generosMusicales = db.TB_GenerosMusicales.ToList();
+
+                    if (generosMusicales.Any())
+                    {
+                        listaGenerosMusicales = generosMusicales.Select(x => new GenerosMusicalesViewModel
+                        {
+                            CodigoGenero = x.CodigoGenero,
+                            Descripcion = x.Descripcion,
+                            Imagen = String.IsNullOrEmpty(x.Imagen) ? Url.Content("~/Content/Images/DefaultAlbum.png") : x.Imagen,
+                        }).ToList();                      
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(ex);
+            }
+
+            return View(model: listaGenerosMusicales);
+        }
+    }
+}
---
+++ VentaMusical/Models/ViewModels/GenerosMusicalesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace VentaMusical.Models.ViewModels
+{
+    public class GenerosMusicalesViewModel
+    {
+        public int CodigoGenero { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+    }
+}
