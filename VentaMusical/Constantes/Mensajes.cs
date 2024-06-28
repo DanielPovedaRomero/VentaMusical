c44e8603ab6303e93b30f24511b87ebfdddd8333
Daniel Poveda <danielpovedaromero@gmail.com>
2024-06-27 23:40:31 -0600

    Se agrega sweet alert y modal de registro de generos de canciones

---
+++ VentaMusical/Constantes/Mensajes.cs
@@ -0,0 +1,8 @@
+﻿namespace VentaMusical.Constantes
+{
+    public static class Mensajes
+    {
+        public static string Error = "Error en la solicitud";
+        public static string Exito = "Registro exitoso";
+    }
+}
--- VentaMusical/Controllers/GenerosMusicalesController.cs
+++ VentaMusical/Controllers/GenerosMusicalesController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Web;
 using System.Web.Mvc;
+using VentaMusical.Constantes;
 using VentaMusical.Models;
 using VentaMusical.Models.ViewModels;
 
@@ -38,6 +42,31 @@ namespace VentaMusical.Controllers
             }
 
             return View(model: listaGenerosMusicales);
+        }
+
+        [HttpPost]
+        public ActionResult Guardar(string descripcion, HttpPostedFileBase imagen)
+        {
+            try
+            {
+                using (VentaMusicalDBEntities db = new VentaMusicalDBEntities())
+                {
+                    TB_GenerosMusicales genero = new TB_GenerosMusicales()
+                    {
+                        Descripcion = descripcion,
+                        Imagen = imagen.ToString(),
+                    };
+
+                    db.TB_GenerosMusicales.Add(genero);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json( new RespuestaModel { Codigo = HttpStatusCode.InternalServerError, Mensaje = Mensajes.Error, Resultado = false });
+            }
+
+            return Json( new RespuestaModel { Codigo = HttpStatusCode.InternalServerError, Mensaje = Mensajes.Exito, Resultado = true });
         }
     }
 }
