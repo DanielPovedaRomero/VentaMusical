f8a6e27a13efa763413205fcf5afc92582932481
Kevin Josué Méndez Víquez <mendezviquezkj@gmail.com>
2024-07-28 10:53:13 -0600

    Se crean los metodos en el controlador Usuarios

--- VentaMusical/Constantes/Mensajes.cs
+++ VentaMusical/Constantes/Mensajes.cs
@@ -9,5 +9,9 @@
         public static string ErrorEliminarGenero = "Existen registros de canciones asociados a este género. Por favor, elimine esas canciones antes de eliminar este registro.";
         public static string GeneroConMismaDescripcion = "Ya existe un género musical con esta descripción.";
         public static string CancionConMismaDescripcion = "Ya existe una canción con ese nombre asociada a este género musical.";
+        public static string ErrorEliminarUsuario = "No existen de usuarios asociados a este número de Identificación. Por favor verificar si el número de Identificación es el correcto";
+        public static string UsuariosConMismaCedula = "Ya existe un usuario registrado con ese número de cédula en el sistema";
+        public static string CorreoNoValido = "La dirección de correo electronico no cumple con las politicas establecidas";
+
     }
 }
---
+++ VentaMusical/Models/ViewModels/UsuarioModels/EditUsuario.cs
@@ -0,0 +1,15 @@
+﻿
+namespace VentaMusical.Models.ViewModels.UsuarioModels
+{
+    public class EditUsuario
+    {
+        public int NumeroIdentificacion { get; set; }
+        public string Nombre { get; set; }
+        public int IdGenero { get; set; }
+        public string Correo { get; set; }
+        public int IdTarjeta { get; set; }
+        public string NumeroTarjeta { get; set; }
+        public string Contrasena { get; set; }
+        public int IdPerfil { get; set; }
+    }
+}
